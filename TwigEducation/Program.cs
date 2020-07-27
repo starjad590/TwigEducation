3d9ab10ef5108d9c17ca1d5e75a55bce479c1f22
jordan.davidson <jordan.davidson@live.co.uk>
2020-07-28 00:33:28 +0100

    Changed the provided numbers
    Changed the provided numbers back to the requested ones in the brief

--- TwigEducation/Program.cs
+++ TwigEducation/Program.cs
@@ -7,8 +7,8 @@ namespace TwigEducation
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[] { 1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29 };
-            int split = 7;
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+            int split = 3;
 
             IArrayDesigner Designer = new ArrayDesigner(numbers, split);
 
